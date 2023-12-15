d622630f80467f1ac43f4037eee66734f1ec7b68
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-15 07:06:07 +0100

    Refactor softmax function to handle batch inputs

--- packages/scripts/python/neural-networks/np_why_softmax.py
+++ packages/scripts/python/neural-networks/np_why_softmax.py
@@ -1,18 +1,17 @@
-import math 
+
 import numpy as np
 import nnfs
 
 nnfs.init()
 
-layer_outputs = [4.8, 1.21, 2.385]
+layer_outputs = [[4.8, 1.21, 2.385],
+                 [8.9, -1.81, 0.2],
+                 [1.41, 1.051, 0.026]]
 
-# E = 2.71828182846
-E = math.e
+exp_values = np.exp(layer_outputs) # we dont need to do anything to handle the batch, numpy.exp() does this by default
 
-exp_values = np.exp(layer_outputs)
-    
 print("exponentiated values: \n", exp_values, "\n")
 
-norm_values = exp_values / np.sum(exp_values)
+norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
      
 print("normalized exponentiated values: \n", norm_values, "\n")
