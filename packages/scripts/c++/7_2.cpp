e9c014fc77a44c44446c8539812dc7fd9dd87200
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-15 22:33:23 +0100

    Refactor average and median calculation, remove trailing zeroes in average output

--- packages/scripts/c++/7_2.cpp
+++ packages/scripts/c++/7_2.cpp
@@ -13,32 +13,51 @@ int main() {
     }
 
     if (numbers.empty()) {
-        std::cout << "No numbers were entered." << std::endl;
+        std::cout << "No numbers were entered." << std::ends;
         return 0;
     }
 
-    // Calculate average
     double average = std::accumulate(numbers.begin(), numbers.end(), 0.0) / numbers.size();
 
-    // Calculate median
     std::nth_element(numbers.begin(), numbers.begin() + numbers.size() / 2, numbers.end());
     double median = numbers[numbers.size() / 2];
     if (numbers.size() % 2 == 0) {
         median = (median + *std::max_element(numbers.begin(), numbers.begin() + numbers.size() / 2)) / 2.0;
     }
 
-    // Sort in descending order
     std::sort(numbers.begin(), numbers.end(), std::greater<int>());
 
-    // Output
-    std::cout << std::fixed << std::setprecision(4);
-    std::cout << "Average: " << average << std::endl;
-    std::cout << "Median: " << median << std::endl;
-    std::cout << "Descending: ";
+    // average. it is annoying as hell to remove traling zeroes
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(4) << average; // Write the average to the stringstream
+    std::string str = ss.str(); // Extract the string
+
+// Ensure that there is a decimal point somewhere (there should be)
+    if(str.find('.') != std::string::npos)
+    {
+        // Remove trailing zeroes
+        str = str.substr(0, str.find_last_not_of('0')+1);
+        // If the decimal point is now the last character, remove that as well
+        if(str.back() == '.')
+        {
+            str.pop_back();
+        }
+    }
+    std::cout << "Average : " << str << std::endl;
+
+
+
+    // Check if the median is a whole number
+    if (median == static_cast<int>(median)) {
+        std::cout << "Median : " << static_cast<int>(median) << std::endl;
+    } else {
+        std::cout << "Median : " << median << std::endl;
+    }
+
+    std::cout << "Descending : ";
     for (const auto& n : numbers) {
         std::cout << n << " ";
     }
     std::cout << std::endl;
-
     return 0;
 }
