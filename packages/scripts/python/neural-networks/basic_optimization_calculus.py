44bca2dbd952b2f5e866234b58411bfeadf759fc
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-17 03:34:49 +0100

    Add approximate tangent lines to the plot

--- packages/scripts/python/neural-networks/basic_optimization_calculus.py
+++ packages/scripts/python/neural-networks/basic_optimization_calculus.py
@@ -59,34 +59,36 @@ x = np.arange(0, 50, 0.001)
 y = f(x)
 
 plt.plot(x, y, label='y = f(x)')
+colors = ['r', 'g', 'b', 'y', 'm', 'c', 'k', 'w']
 
-p2_delta = 0.0001
-x1 = 20
-x2 = x1 + p2_delta
-
-y1 = f(x1)
-y2 = f(x2)
-
-print((x1, y1), (x2, y2))
-
-approximate_derivative = (y2 - y1) / (x2 - x1)
-print(approximate_derivative)
-
-b = y2 - approximate_derivative * x2
-
-def approximate_tangent_line(x):
+def approximate_tangent_line(x, approximate_derivative, b):
     return approximate_derivative * x + b
 
-to_plot = [x1 - 10.9, x1, x1 + 10.9]
-plt.plot(to_plot,
-        [approximate_tangent_line(i) for i in to_plot],
-        label='Approximate derivative line')
+for i in range(10):
+    p2_delta = 0.0001
+    x1 = i
+    x2 = x1 + p2_delta
 
-print('Approximate derivative for f(x)',
-        f'where x = {x1} is {approximate_derivative}')
+    y1 = f(x1)
+    y2 = f(x2)
 
-plt.plot(x1, y1, 'ro', label=f'y = f({x1})')  # 'ro' for red circle marker
-plt.plot(x2, y2, 'go', label=f'y = f({x2})')  # 'go' for green circle marker
+    print((x1, y1), (x2, y2))
+
+    approximate_derivative = (y2 - y1) / (x2 - x1)
+    print(approximate_derivative)
+
+    b = y2 - approximate_derivative * x2
+
+    to_plot = [x1 - 10.9, x1, x1 + 10.9]
+    random_color = np.random.randint(0, 8)
+    plt.scatter(x1, y1, c=colors[random_color])
+    plt.plot(to_plot,
+            [approximate_tangent_line(point, approximate_derivative, b) for point in to_plot],
+            label='Approximate derivative line',
+            c=colors[random_color])
+
+    print('Approximate derivative for f(x)',
+            f'where x = {x1} is {approximate_derivative}')
 
 plt.xlabel('x')
 plt.ylabel('y')
--- packages/scripts/python/neural-networks/run.sh
+++ packages/scripts/python/neural-networks/run.sh
@@ -2,3 +2,4 @@ python -m venv venv
 source venv/Scripts/activate
 pip install -r requirements.txt
 python ReLU_activation_function.py
+
