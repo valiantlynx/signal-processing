d1e5eb03a68495902e9bf6ee0b31da98608d9922
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-05 02:59:47 +0100

    Squashed 'apps/blog-svelte/' changes from c390c7da..16631f62
    16631f62 Merge commit 'c38d35c4c38ac90b061c76f386f1284fb6657181' into valiantlynx
    f5a7626d Update meta tags and site tags
    44d813f9 Fix bug in login functionality
    8dc706d0 Update text in blog page component
    a9b2fd39 Add Share component and update meta tags for social media sharing
    735d2fdd Update link colors in BlogCards.svelte
    f2a17c2c Refactor LikeButton and +page.server.js
    324e1413 Fix formatting issues and update dependencies
    1ee4b235 Refactor like functionality and add like count cant create likes without error yet
    9d01bc14 Refactor blog page component to include like functionality
    efb212ad Update tag link styling
    6b484a54 Update tag name in All Blogs header
    d6127ac7 Fix filter bug in load function
    fe0114a7 Add image URL to blog object and display tag in page title
    9d8c1162 Update tag links to use tag IDs instead of names
    bae22d2c Remove unused code related to blog pages and project editing
    cfc509cb Fix expand property in load function and update blog tags display
    be829e48 Remove console.log statement in +page.server.js
    425a12df Refactor blog page component and update dependencies
    
    git-subtree-dir: apps/blog-svelte
    git-subtree-split: 16631f629b485faea89b4a97f4790154c892d022

--- src/routes/[blog]/+page.svelte
+++ src/routes/[blog]/+page.svelte
@@ -1,27 +1,115 @@
 <script>
-	import { page } from '$app/stores';
-	import { ValiantRichText, getData } from '@valiantlynx/svelte-rich-text';
-	import { pb } from '$lib/utils/api';
-	import toast from 'svelte-french-toast';
+  	import LikeButton from '$lib/components/blog/LikeButton.svelte';
+    import { page } from '$app/stores';
+    import { ValiantRichText, getData } from '@valiantlynx/svelte-rich-text';
+    import { pb } from '$lib/utils/api';
+    import toast from 'svelte-french-toast';
+    import Chat from '$lib/components/Chat.svelte';
+	import Share from '$lib/components/share/Share.svelte';
 
-  const blog = $page.data.blog;
+    const blog = $page.data.blog;
 
-  const saveData = (data) => {
-	try {
-		console.log(data);
-	const datapb = {
-    "content_object": data
-};
- pb.collection('blogs').update(blog.id, datapb);
- toast.success('Blog post updated successfully');
-	} catch (error) {
-		console.log(error);
-		toast.error('Something went wrong please try again');
-	}
-  };
-
+    const saveData = (data) => {
+        try {
+            console.log(data);
+            const datapb = {
+                "content_object": data
+            };
+            pb.collection('blogs').update(blog.id, datapb);
+            toast.success('Blog post updated successfully');
+        } catch (error) {
+            console.log(error);
+            toast.error('Something went wrong please try again');
+        }
+    };
 </script>
 
+<!-- Blog Container -->
+<div class="container mx-auto px-4 md:px-8 lg:px-12 py-4 md:py-8 lg:py-12">
+<!-- Blog Header -->
+<div class="mb-8">
+    <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">{blog?.title}</h1>
+    
+    <div class="flex items-center gap-4 mb-4">
+        <!-- Author Avatar -->
+        <div class="shrink-0">
+            <img src={blog.expand?.author.avatar ? `https://animevariant.fly.dev/api/files/${blog.expand?.author?.collectionId}/${blog.expand?.author?.id}/${blog.expand?.author?.avatar}`: 'https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=' + blog.expand?.author?.username} alt={"Author" + blog.expand?.author?.username} class="w-12 h-12 rounded-full shadow-lg"/>
+        </div>
+
+        <!-- Author & Metadata -->
+        <div class="flex flex-col md:flex-row gap-2 md:gap-4 text-sm ">
+            <p class="font-semibold">By: {blog?.expand?.author.username}</p>
+            <p>Published: {new Date(blog?.created).toLocaleDateString()}</p>
+            <p>Updated: {new Date(blog?.updated).toLocaleDateString()}</p>
+        </div>
+    </div>
+
+		<!-- Like Button and Form -->
+		<LikeButton />
+
+    <!-- Animated Scroll Down Indicator (Optional) -->
+    <div class="animate-bounce mt-4 flex">
+      
+        <span class="text-xs flex mt-1">Scroll to read</span>
+		<i class="material-icons text-lg ">expand_more</i>
+    </div>
+</div>
+
+
+    <!-- Blog Image -->
+    <img src={blog?.image} alt={blog?.title} class="w-full h-auto rounded-lg mb-6" />
+
+    <!-- Blog Content -->
+    <div class="text-base-content">
+		{#if $page.data.user}
+		{#if $page.data.user.id === blog.author}
+		<ValiantRichText
+		initialData={blog.content_object}
+		 />
+		<button 
+		class="btn btn-primary"
+		on:click={()=>{
+			const data = getData(); // returns dataBlock[] type
+			saveData(data);
+		  }}>Save</button>
+		{:else}
+		<h3 class="text-xl text-accent md:text-2xl lg:text-3xl font-bold mb-4">
+			you can not edit this blog post. as you are not the author of this blog post. Create your own blog post <a href="/blogs/new" class="link link-primary">here</a>
+		</h3>
+		<ValiantRichText initialData={blog.content_object} viewMode={true} />
+		{/if}
+
+{:else}
+<h3 class="text-xl text-accent md:text-2xl lg:text-3xl font-bold mb-4">
+			It is possible to edit this blog post. Please <a href="/login" class="link link-primary">login</a> to edit.
+		</h3>
+<ValiantRichText initialData={blog.content_object} viewMode={true} />
+		{/if}
+    </div>
+
+    <!-- Blog Tags loop through blog?.expand?.tags and display the name-->
+	<div class="flex flex-wrap mt-8">
+		{#each blog?.expand?.tags as tag}
+		<a
+			href="/blogs/tags/{tag.id}"
+			class="bg-base-200 hover:bg-base-300 font-semibold py-2 px-4 rounded-full mr-2 mb-2"
+		>
+			{tag.name}
+		</a>
+		{/each}
+	</div>
+  
+	<Share
+	title={blog.title + ' ' +blog.expand?.author?.username + ' ' + $page.url.hostname}
+	url={$page.url.href}
+	image={blog?.image}
+	text={`read ${blog.title} by ${blog.expand?.author?.username} at ${$page.url.hostname} free online, high quality`}
+	hashtags={blog?.expand?.tags.map((tag) => tag.name)}
+
+/>	
+    <!-- Chat Component -->
+    <Chat class="mt-8" />
+</div>
 
 <svelte:head>
 	<title>{blog.title} | valiantlynx</title>
@@ -49,7 +137,7 @@
 
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content={$page.url.origin} />
-	<!--meta property="og:image" content="/twitter-image.png" /-->
+	<meta property="og:image" content={blog?.image}/>
 	<meta property="og:image:alt" content="{$page.data.siteName} Logo" />
 	<meta property="og:site_name" content={$page.data.siteName} />
 
@@ -57,12 +145,13 @@
 	<meta name="twitter:card" content={blog.image} />
 	<meta name="twitter:title" content={blog.title} />
 	<meta name="twitter:description" content={blog.summary} />
-	<!--meta name="twitter:image" content="/twitter-image.png" /-->
+	<meta name="twitter:image" content={blog?.image} />
 
 	<!-- Google / Search Engine Tags -->
 	<meta itemprop="name" content={blog.title}/>
 
 	<!-- Facebook Meta Tags (for social media sharing) -->
+	<meta property="fb:image" content={blog.image} />
 	<meta property="fb:app_id" content={$page.data.siteName} />
 	<meta property="fb:admins" content={$page.data.siteName} />
 	<meta property="fb:page_id" content={$page.data.siteName} />
@@ -82,7 +171,7 @@
 	<!-- Schema.org Meta Tags (for SEO) -->
 	<meta itemprop="headline" content={blog.title} />
 	<meta itemprop="description" content={blog.summary} />
-	<!--meta itemprop="image" content="/twitter-image.png" /-->
+	<meta itemprop="image" content={blog?.image} />
 
 	{#if $page.data.sites}
 		<!-- clarity there is abug in svelte where inside the svript tags i cannot access the variables //! https://stackoverflow.com/questions/63419284/svelte-substitution-in-script-within-sveltehead-->
@@ -129,51 +218,4 @@
 		></script>
 	{/if}
 </svelte:head>
-
-
-
-<div class="p-4 md:p-8 lg:p-12 flex justify-center">
-	<div class="w-3/4">
-		<h1 class="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
-			{blog?.title}
-		</h1>
-		<img
-			src={blog?.image}
-			alt={blog?.title}
-			class="w-full h-auto rounded-lg mb-4"
-		/>
-    <p class="text-sm text-accent">By: {blog?.expand?.author.username}</p>
-    <p class="text-sm text-accent">Published: {blog?.created}</p>
-    <p class="text-sm text-accent">Updated: {blog?.updated}</p>
-    <div class="mt-4 progress-primary">
-		{#if $page.data.user}
-		{#if $page.data.user.id === blog.author}
-		<ValiantRichText
-		initialData={blog.content_object}
-		 />
-		<button 
-		class="btn btn-primary"
-		on:click={()=>{
-			const data = getData(); // returns dataBlock[] type
-			saveData(data);
-		  }}>Save</button>
-		{:else}
-		<h3 class="text-xl text-accent md:text-2xl lg:text-3xl font-bold mb-4">
-			you can not edit this blog post. as you are not the author of this blog post. Create your own blog post <a href="/blogs/new" class="link link-primary">here</a>
-		</h3>
-		<ValiantRichText initialData={blog.content_object} viewMode={true} />
-		{/if}
-
-{:else}
-<h3 class="text-xl text-accent md:text-2xl lg:text-3xl font-bold mb-4">
-			It is possible to edit this blog post. Please <a href="/login" class="link link-primary">login</a> to edit.
-		</h3>
-<ValiantRichText initialData={blog.content_object} viewMode={true} />
-		{/if}
-    </div>
-
-		<p class="text-sm text-secondary">Tags: {blog?.tags}</p>
-	
-	</div>
-</div>
 
