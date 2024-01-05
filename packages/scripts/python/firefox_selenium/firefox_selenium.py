1b983fbb3e01b85ac8801a88336878ca2debe550
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-05 09:52:25 +0100

    Add Firefox Selenium driver and update script

--- packages/scripts/python/firefox_selenium/firefox_selenium.py
+++ packages/scripts/python/firefox_selenium/firefox_selenium.py
@@ -1,2 +1,11 @@
 from selenium import webdriver
-print("Hello World")
+from selenium.webdriver.firefox.service import Service
+
+# Create a Service object with the executable path (GeckoDriver)
+service = Service(r'.\geckodriver-v0.34.0-win64\geckodriver.exe')
+
+# Initialize the Firefox driver with the Service object
+driver = webdriver.Firefox(service=service)
+
+
+driver.get('https://github.com')
