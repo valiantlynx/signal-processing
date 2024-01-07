30716d375f7b0594508f1c6f024bbf96a099f52f
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-07 23:58:14 +0100

    Update SvelteHead component with dynamic title, image, keywords, and description

--- apps/svelte-manga/src/lib/components/SvelteHead.svelte
+++ apps/svelte-manga/src/lib/components/SvelteHead.svelte
@@ -1,38 +1,42 @@
 <script>
     import { page } from '$app/stores';
+    export let title = $page.data.siteName
+    export let image = "/twitter-image.png"
+    export let keywords = "manga, read manga, read manga online, request manga, manga reading website, populate manga, manga online, manga, manga details, manga chapters, read manga free, manga free, read free manga, read free manga online, manga reader, manga viewer, manga scans, {$page.data.siteName}, {$page.url.origin} ";
+    export let description = 'Read the latest manga online for free at AnimeVariant, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters.';
 </script>
 
 
 <svelte:head>
-	<title>{$page.data.siteName} manga page</title>
+	<title> {title}</title>
 	<!-- Open Graph Meta Tags (for social media sharing) -->
-	<meta property="og:title" content="{$page.data.siteName}" />
+	<meta property="og:title" content={title} />
 	<meta
 		property="og:description"
-		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
+		content={description}
 	/>
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content={$page.url.origin} />
-	<meta property="og:image" content="/twitter-image.png" />
+	<meta property="og:image" content={image} />
 	<meta property="og:image:alt" content="{$page.data.siteName} Logo" />
-	<meta property="og:site_name" content="{$page.data.siteName}" />
+	<meta property="og:site_name" content={title} />
 
 	<!-- Twitter Meta Tags (for social media sharing) -->
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content="{$page.data.siteName}" />
+	<meta name="twitter:title" content={title} />
 	<meta
 		name="twitter:description"
-		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
+		content={description}
 	/>
-	<meta name="twitter:image" content="/twitter-image.png" />
+	<meta name="twitter:image" content={image} />
 
 	<!-- Google / Search Engine Tags -->
-	<meta itemprop="name" content="{$page.data.siteName}" />
+	<meta itemprop="name" content={title} />
 	<meta
 		itemprop="description"
-		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
+		content={description}
 	/>
-	<meta itemprop="image" content="/twitter-image.png" />
+	<meta itemprop="image" content={image} />
 
 	<!-- Facebook Meta Tags (for social media sharing) -->
 	<meta property="fb:app_id" content="AnimeVariant" />
@@ -51,19 +55,16 @@
 	<meta property="article:author:username" content="{$page.data.siteName}" />
 
 	<!-- Schema.org Meta Tags (for SEO) -->
-	<meta itemprop="headline" content="{$page.data.siteName}" />
+	<meta itemprop="headline" content={title} />
 	<meta
 		itemprop="description"
-		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
+		content={description}
 	/>
-	<meta itemprop="image" content="/twitter-image.png" />
+	<meta itemprop="image" content={image} />
 	<!-- Keywords Meta Tag -->
 	<meta
 		name="keywords"
-		content="manga, read manga, read manga online, request manga, manga reading website, populate manga, manga online, manga, manga details, manga chapters, read manga free, manga free, read free manga, read free manga online, manga reader, manga viewer, manga scans, {$page.data.siteName}, {$page.url.origin} "
+		content={keywords}
 	/>
-	<meta
-		name="description"
-		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
-	/>
+    <meta name="description" content={description} />
 </svelte:head>
