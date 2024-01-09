7cffb509a9c2ceb71770e417c6da6b6f5e4ab30a
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-09 23:53:31 +0100

    Add logo to Footer component

--- apps/svelte-manga/src/lib/components/Footer.svelte
+++ apps/svelte-manga/src/lib/components/Footer.svelte
@@ -1,9 +1,18 @@
 <script>
 	import { site } from '@valiantlynx/general-config';
+	import { page } from '$app/stores';
 	import Icon from '@iconify/svelte';
+	const logo = $page.data.sites?.logo
+                ? `${site.site.pocketbase}/api/files/${$page.data.sites?.collectionId}/${$page.data.sites?.id}/${$page.data.sites?.logo}`
+                : `https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=${$page.data.sites?.siteName}`;
 </script>
 
 <footer class="footer footer-center p-10 bg-base-300 text-base-content rounded mt-auto">
+	<a href="/" class="p-1">
+		{#if !$page.url.isSmallDevice}
+				<img src={logo} alt="{$page.data.siteName} logo"  class="w-20" />
+		{/if}
+</a>
 	<div class="grid grid-flow-col gap-4">
 		<a href="/about" class="link link-hover">About us</a>
 		<a href="/contact" class="link link-hover">Contact</a>
