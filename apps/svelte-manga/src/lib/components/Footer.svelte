564f1d907d5e57879ebc7edc21bcae0c116ae752
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-09 23:56:21 +0100

    Update copyright year in Footer component

--- apps/svelte-manga/src/lib/components/Footer.svelte
+++ apps/svelte-manga/src/lib/components/Footer.svelte
@@ -33,6 +33,6 @@
 		</div>
 	</div>
 	<div>
-		<p>Copyright © 2023 - All right reserved by {site.site.company}</p>
+		<p>Copyright © {new Date().getFullYear()} - All right reserved by {site.site.company}</p>
 	</div>
 </footer>
