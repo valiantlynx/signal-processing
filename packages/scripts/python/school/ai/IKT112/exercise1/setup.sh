f12a0abe997da9f0735466dcf706228b5c8a0268
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-16 09:18:18 +0100

    Refactor code for improved performance

---
+++ packages/scripts/python/school/ai/IKT112/exercise1/setup.sh
@@ -0,0 +1,3 @@
+git subtree add --prefix=packages/scripts/python/signal-processing https://github.com/valiantlynx/signal-processing.git main --squash
+git subtree pull --prefix=packages/scripts/python/signal-processing https://github.com/valiantlynx/signal-processing.git main --squash
+git subtree push --prefix=packages/scripts/python/signal-processing https://github.com/valiantlynx/signal-processing.git main
