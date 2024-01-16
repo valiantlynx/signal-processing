481cbbff73fa30b207d11eb0aa0e621d1e6b76ea
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-16 09:24:06 +0100

    Add Calculator class and basic arithmetic operations

--- packages/scripts/python/school/ai/IKT112/exercise1/basic-arithmetic-calculator.py
+++ packages/scripts/python/school/ai/IKT112/exercise1/basic-arithmetic-calculator.py
@@ -1,1 +1,14 @@
 # Task: Create a Python program that takes two numbers as input and allows the user to choose to add, subtract, multiply, or divide them.
+
+# class for the calculator
+class Calculator:
+    def __init__(self, num1, num2) -> None:
+        self.num1 = num1
+        self.num2 = num2
+    
+    def add(self):
+        return 
+
+# input from user for two numbers
+
+# input from user for operation
